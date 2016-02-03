1b7c62af1505806a6d4cebb01b108c896e82133f
Yoan Blanc <yoan@dosimple.ch>
2016-02-03 16:35:15 +0100

    Code is evaluating.

--- MainForm.cs
+++ MainForm.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Dynamic;
 using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
 #if WINDOWS 
 using ScintillaNET;
 #endif
-//using ExpressionEvaluator;
+using ExpressionEvaluator;
 
 
 namespace diese
@@ -14,16 +15,17 @@ namespace diese
     {
         public const string VERSION = "0.0.1";
 
-        private Diese diese;
-        private Background background;
+        private Diese dièse;
+        private Background fond;
         private Canvas canvas;
+        private Control éditeur;
         private long frame;
 
         public MainForm(Diese diese)
         {
-            this.diese = diese;
+            this.dièse = diese;
 
-            background = new Background();
+            fond = new Background();
 
             canvas = new Canvas();
             canvas.AddActor(diese);
@@ -45,6 +47,9 @@ namespace diese
             var splitter = new SplitContainer();
             splitter.Location = zero;
             splitter.Size = size;
+            //splitter.AutoSize = true;
+            splitter.Dock = DockStyle.Fill;
+            splitter.Parent = this;
             splitter.Anchor = (AnchorStyles)(
                 AnchorStyles.Top |
                 AnchorStyles.Right |
@@ -58,13 +63,16 @@ namespace diese
             // TODO.
 #else
             var scintilla = new TextBox();
+            scintilla.WordWrap = true;
             scintilla.Multiline = true;
             scintilla.ScrollBars = ScrollBars.Both;
             scintilla.Font = new Font("Mono", 15, FontStyle.Regular);
 #endif
-
+            scintilla.Dock = DockStyle.Fill;
+            scintilla.AutoSize = true;
             scintilla.Location = zero;
             scintilla.Size = size;
+            scintilla.Parent = splitter.Panel1;
             scintilla.Anchor = (AnchorStyles)(
                 AnchorStyles.Top |
                 AnchorStyles.Right |
@@ -73,10 +81,13 @@ namespace diese
             );
 
 
-            background.Cursor = Cursors.Default;
-            background.Location = zero;
-            background.Size = size;
-            background.Anchor = (AnchorStyles)(
+            fond.Cursor = Cursors.Default;
+            fond.Dock = DockStyle.Fill;
+            fond.Parent = splitter.Panel2;
+            fond.AutoSize = true;
+            fond.Location = zero;
+            fond.Size = size;
+            fond.Anchor = (AnchorStyles)(
                 AnchorStyles.Top |
                 AnchorStyles.Right |
                 AnchorStyles.Bottom |
@@ -84,6 +95,8 @@ namespace diese
             );
 
             canvas.Cursor = Cursors.Default;
+            canvas.Dock = DockStyle.Fill;
+            canvas.AutoSize = true;
             canvas.Location = zero;
             canvas.Size = size;
             canvas.Anchor = (AnchorStyles)(
@@ -93,16 +106,20 @@ namespace diese
                 AnchorStyles.Left
             );
 
+            // lien.
+            éditeur = scintilla;
+
             // Les poupées russes.
-            background.Controls.Add(canvas);
-            splitter.Panel1.Controls.Add(scintilla);
-            splitter.Panel2.Controls.Add(background);
+            fond.Controls.Add(canvas);
+            splitter.Panel1.Controls.Add(éditeur);
+            splitter.Panel2.Controls.Add(fond);
             Controls.Add(splitter);
         }
 
         private void InitializeEvents()
         {
-            background.KeyUp += onKeyUp;
+            éditeur.KeyUp += onKeyUp;
+            fond.KeyUp += onKeyUp;
 
             var tick = new Timer();
             tick.Interval = 1000 / 60; // 60 fps.
@@ -115,20 +132,73 @@ namespace diese
         private void onKeyUp(object sender, KeyEventArgs e) {
             var pas = 100;
    
-            switch (e.KeyCode)
+            if (sender == fond)
+                switch (e.KeyCode)
+                {
+                    case Keys.Left:
+                        dièse.Move(-pas, 0);
+                        break;
+                    case Keys.Right:
+                        dièse.Move(pas, 0);
+                        break;
+                    case Keys.Down:
+                        dièse.Move(0, pas);
+                        break;
+                    case Keys.Up:
+                        dièse.Move(0, -pas);
+                        break;
+                    case Keys.F5:
+                        evaluate(éditeur.Text);
+                        break;
+                }
+            if (sender == éditeur)
+                switch (e.KeyCode)
+                {
+                    case Keys.F5:
+                        evaluate(éditeur.Text);
+                        break;
+                }
+        }
+
+        private void evaluate(string code)
+        {
+            var registry = new TypeRegistry();
+            registry.RegisterDefaultTypes();
+            registry.RegisterType("Console", typeof(Console));
+            dynamic scope = new ExpandoObject();
+            scope.gauche = (Func<int,ExpandoObject>)(x =>
+                {
+                    dièse.Move(-x, 0);
+                    return scope;
+                });
+            scope.droite = (Func<int,ExpandoObject>) (x =>
+                {
+                    dièse.Move(x, 0);
+                    return scope;
+                });
+            scope.haut = (Func<int,ExpandoObject>)(x =>
+                {
+                    dièse.Move(0, -x);
+                    return scope;
+                });
+            scope.bas = (Func<int,ExpandoObject>) (x =>
+                {
+                    dièse.Move(0, x);
+                    return scope;
+                });
+            var expression = new CompiledExpression(code)
             {
-                case Keys.Left:
-                    diese.Move(-pas, 0);
-                    break;
-                case Keys.Right:
-                    diese.Move(pas, 0);
-                    break;
-                case Keys.Down:
-                    diese.Move(0, pas);
-                    break;
-                case Keys.Up:
-                    diese.Move(0, -pas);
-                    break;
+                TypeRegistry = registry
+            };
+            expression.ExpressionType = CompiledExpressionType.StatementList;
+
+            try {
+                var f = expression.ScopeCompile<ExpandoObject>();
+                f(scope);
+            } catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException e) {
+                MessageBox.Show(e.Message);
+            } catch (ExpressionEvaluator.Parser.ExpressionParseException e) {
+                MessageBox.Show(e.Message);
             }
         }
 
